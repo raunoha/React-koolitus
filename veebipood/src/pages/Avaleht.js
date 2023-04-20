5b7308ee1c4f9d9b7d920d8325a18dea7a140310
raunoha <129747825+raunoha@users.noreply.github.com>
2023-04-20 12:05:21 +0300

    usestare, useref, dünaamiline veebipood 20.04.23

--- veebipood/src/pages/Avaleht.js
+++ veebipood/src/pages/Avaleht.js
@@ -1,9 +1,43 @@
 //rfce
-import React from 'react'
+import React, { useState } from 'react'
 
 function Avaleht() {
+    const [kogus,uuendaKogus] = useState(12);
+    const [syda, uuendaSyda] = useState(false); // 0
+    const [sonum, uuendaSonum] = useState("Uuenda kogust!");
+
+    function nulli(){
+        uuendaKogus(0);
+        uuendaSonum("Panid tagasi nulli!");
+    }
+
+    function vahenda(){
+        uuendaKogus(kogus - 1);
+        uuendaSonum("Vähenda kogust!");
+    }
+
+    function suurenda(){
+         uuendaKogus(kogus + 1);
+        uuendaSonum("Suurendasid kogust!");
+    }
+
   return (
-    <div>Avaleht</div>
+    <div>
+      {syda=== true && <img src="/syda.svg" alt="" />}
+      {syda === false &&<img src="/syda mittelaigitud.svg" alt="" />}
+<span>{syda + 0}</span>
+<button onClick={() => uuendaSyda(true)}>Muuda laigituks</button>
+<button onClick={() => uuendaSyda(false)}>Muuda mittelaigitud</button>
+
+<br/>
+<br/>
+
+<div>{sonum}</div>
+        {kogus!== 0 && <button onClick={nulli}>Tagasi nulli</button>}
+        <button onClick={vahenda}>-</button>
+        <span>kogus:{kogus}tükki </span>
+        <button onClick={suurenda}>+</button>
+    </div>
   )
 }
 
--- veebipood/src/pages/Lisatoode.js
+++ veebipood/src/pages/Lisatoode.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useRef,useState } from 'react'
 
 function Lisatoode() {
+    const [sonum, uuendaSonum] = useState("Lisa toode!");
+const inputiLuger = useRef();  //ref loeb inputi seest väärtust
+
+//const nimiRef = useRef
+// const nimiViide = useRef()
+    //function lisa () {}
+
+    const lisa = () => {
+        uuendaSonum("Toode lisatud, nimega: "+ inputiLuger.current.value);
+        
+    }
+
   return (
-    <div>Lisatoode</div>
+    <div>
+
+    
+    <div>{sonum}</div>
+    <label htmlFor="">Uue toote nimi</label><br />
+    <input ref={inputiLuger} type="text" /> <br />
+    <button onClick={lisa}>Lisa</button> <br />
+    </div>
   )
 }
 
-export default Lisatoode
+export default Lisatoode;
